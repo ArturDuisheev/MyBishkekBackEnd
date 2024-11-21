e2d3568b5b167b5a9bc1c4b28818949fd29fc788
ArturDuisheev <myworkingartur@gmail.com>
2024-11-22 03:46:09 +0600

    sf

--- backend/api_solution/services.py
+++ backend/api_solution/services.py
@@ -7,31 +7,9 @@ class WeatherService:
     API_KEY = "8ed6c0f2b978147e0b139b888ea46349"
 
     @staticmethod
-    def get_client_ip(request):
-        """
-        Получить IP-адрес пользователя из запроса.
-        Если сервер находится за прокси, будем использовать 'HTTP_X_FORWARDED_FOR'.
-        """
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        if x_forwarded_for:
-            ip = x_forwarded_for.split(',')[0]  # Первый IP в списке (если прокси)
-        else:
-            ip = request.META.get('REMOTE_ADDR')  # Обычный IP
-
-        return ip
-
-    staticmethod
-
-    def get_coordinates_by_ip(ip=None):
-        """
-        Получить координаты (широту и долготу) по IP.
-        Если IP не передан, будет использован IP, определяемый через заголовок 'REMOTE_ADDR'.
-        """
-        if ip is None:
-            raise ValueError("IP-адрес не был передан.")
-
+    def get_coordinates_by_ip(ip):
         try:
-            response = requests.get(f"{WeatherService.IP_API_URL}{ip}")
+            response = requests.get(f"{WeatherService.IP_API_URL}{ip or ''}")
             if response.status_code == 200:
                 data = response.json()
                 if data["status"] == "success":
