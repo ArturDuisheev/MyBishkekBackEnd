420d925f8af6eb745455815d315b02a0565f2bf1
ArturDuisheev <myworkingartur@gmail.com>
2024-11-22 04:20:22 +0600

    s

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,12 +5,19 @@ from .helpers.djoser.urls import urlpatterns as djoser_urls
 from .swagger import urlpatterns as swagger_urls
 
 api_urlpatterns = [
-    path('api/v1/', include('place.urls')),
-    path('api/v1/', include('api_solution.urls')),
-    path('api/v1/', include(djoser_urls)),
+    path('place/', include('place.urls')),
+    path('api_solution/', include('api_solution.urls')),
+    path('auth/', include(djoser_urls)),
     path('swagger/', include(swagger_urls)),
 ]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + api_urlpatterns
+    path('api/v1/', include(api_urlpatterns))
+]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
